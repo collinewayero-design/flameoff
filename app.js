90ea9617e318e68049b8d56b70e6a459e5f9c102
collinewayero-design <collinewayero@gmail.com>
2025-12-18 13:36:40 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
+// Paste your Firebase config here
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "flameoff.firebaseapp.com",
+  projectId: "flameoff",
+  storageBucket: "flameoff.appspot.com",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
+const storage = firebase.storage();
 
